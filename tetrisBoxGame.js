42d915ed88b6c0fd8cf6887ea0661595f1e85ebb
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-05-15 15:30:54 +0900

    goalRemove90 = 0

--- tetrisBoxGame.js
+++ tetrisBoxGame.js
@@ -82,6 +82,7 @@ var tetrisBoxGame = function(){
 		"start":function(){
 			ttr.stop()
 			ttr.clear()
+			ttr.goalRemove90 = 0;
 			ttr.reset()
 			// this.makeStage();
 			this.ab.stop().clear().contentText('Ready',0).show(0,'none')
